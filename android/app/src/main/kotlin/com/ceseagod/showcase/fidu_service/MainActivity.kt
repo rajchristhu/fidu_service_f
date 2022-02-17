8d6bd7585c55d0217b141be01c6d9f5e49237a6b
Christhu <christhurajan@tandemcrystals.com>
2022-02-17 09:48:46 +0530

    init

--- android/app/src/main/kotlin/com/ceseagod/showcase/fidu_service/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.ceseagod.showcase.fidu_service
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/util/textColor.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'theme.dart';
-
-TextStyle titleDefaultTextStyle =  TextStyle(color: whiteColor, fontSize: 20.0, fontWeight: FontWeight.w600);
-TextStyle titleMediumDefaultTextStyle =  TextStyle(color: blackColor, fontSize: 16.0, fontWeight: FontWeight.w500);
-TextStyle paragraphDefaultTextStyle =  TextStyle(color: blackColor, fontSize: 14.0, fontWeight: FontWeight.w500);
-TextStyle paragraphRegularDefaultTextStyle =  TextStyle(color: blackColor, fontSize: 14.0, fontWeight: FontWeight.w400);
